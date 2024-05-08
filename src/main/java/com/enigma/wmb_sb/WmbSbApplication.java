f5697d1ef50f14596aa70e5f6c21a333b5848e84
Sheila Nuur Ditrie <snditrie@gmail.com>
2024-05-08 23:00:08 +0700

    Initial commit

---
+++ src/main/java/com/enigma/wmb_sb/WmbSbApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.wmb_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmbSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmbSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/wmb_sb/WmbSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.wmb_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmbSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
