52c8d96fb92e4a51267bebc826ce7f64db0806e8
Sheila Nuur Ditrie <snditrie@gmail.com>
2024-06-09 23:59:08 +0700

    [CHANGE] deleting path id for update menu and creating HELP.md but not finished yet

--- src/main/java/com/enigma/wmb_sb/controller/MenuController.java
+++ src/main/java/com/enigma/wmb_sb/controller/MenuController.java
@@ -93,7 +93,7 @@ public class MenuController {
         return ResponseEntity.ok(response);
     }
 
-    @PutMapping(path = APIurl.PATH_VAR_ID)
+    @PutMapping
     public ResponseEntity<CommonResponse<MenuResponse>> updateMenu(@RequestBody UpdateMenuRequest request){
         MenuResponse updateMenu = menuService.update(request);
         CommonResponse<MenuResponse> response = CommonResponse.<MenuResponse>builder()
